eeafbdd924e97c30cf5c016901cbd57316778b5d
Cris John Alonzaga <Cris John Alonzaga@DESKTOP-ECR8315>
2024-02-17 23:30:48 +0800

    test commit

--- src/main/java/com/opms/controllers/manage/ActivityController.java
+++ src/main/java/com/opms/controllers/manage/ActivityController.java
@@ -38,7 +38,7 @@ public class ActivityController extends BaseController{
 		
 		model.addAttribute("user", getCurrentUser() );
 		//List<ActivityDto> activities = activityService.findAllActivityByUserPaging( getCurrentUser().getId() , new PaginationUtil(offSet , limit , keyword));
-		//Add comments
+		//Add comm
 		//model.addAttribute("activities", activities);
 		
 		return "/admin/manage/activities";
